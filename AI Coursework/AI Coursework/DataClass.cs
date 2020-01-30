0e20243f4d2dc29cf93ace170b2175a7f5018c88
1700485 <55248329+1700485@users.noreply.github.com>
2020-01-30 17:22:34 +0000

    Created team class

---
+++ AI Coursework/AI Coursework/DataClass.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AI_Coursework
+{
+    public class teamData
+    {
+        string teamName;
+        double recordPercentage;
+        Boolean home;
+
+        //Setters
+        public void setTeamName(string name)
+        {
+            teamName = name;
+        }
+
+        public void setRecordPercentage(double percentage)
+        {
+            recordPercentage = percentage;
+        }
+
+        public void setHome(Boolean Home)
+        {
+            home = Home;
+        }
+
+        //Getters
+        public string getTeamName()
+        {
+            return teamName;
+        }
+
+        public double getRecordPercentage()
+        {
+            return recordPercentage;
+        }
+
+        public Boolean getHome()
+        {
+            return home;
+        }
+    }
+}
--- AI Coursework/AI Coursework/Form1.cs
+++ AI Coursework/AI Coursework/Form1.cs
@@ -19,20 +19,45 @@ namespace AI_Coursework
 
         private void submitButton_Click(object sender, EventArgs e)
         {
+            //Creating instance for each team, data will be passed to AI
+            teamData teamOneData = new teamData();
+            teamData teamTwoData = new teamData();
+
             string teamOne = teamOneDropdown.SelectedIndex.ToString();
             string teamTwo = teamTwoDropdown.SelectedIndex.ToString();
 
             int TeamOneW = Int32.Parse(teamOneWL.Text);
             int teamTwoW = Int32.Parse(teamTwoWL.Text);
 
+            Boolean teamOneHome = false;
+            Boolean teamTwoHome = false;
+
             if (teamOneCheck.Checked)
             {
-
+                teamOneHome = true;
+                teamTwoHome = false;
             }
             else if (teamTwoCheck.Checked)
             {
-
+                teamTwoHome = true;
+                teamOneHome = false;
             }
+
+            //Inserting inputted data into class instances
+            teamOneData.setTeamName(teamOne);
+            teamOneData.setRecordPercentage();
+            teamOneData.setHome(teamOneHome);
+
+            teamTwoData.setTeamName(teamTwo);
+            teamTwoData.setRecordPercentage();
+            teamTwoData.setHome(teamTwoHome);
+
+            main();
+        }
+
+        void main()
+        {
+            //AI code
         }
     }
 }
